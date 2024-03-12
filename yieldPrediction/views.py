4ae7b408804979bee0f8d168da613a171e812d67
Dasun Tharuka <153586356+dasun-tharuka@users.noreply.github.com>
2024-03-12 19:54:43 +0530

    Yield Prediction Page - Test Code #4

--- yieldPrediction/views.py
+++ yieldPrediction/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
-from joblib import load
+import pandas as pd
 
-# Load the trained model
-model = load('./savedModels/MLPRegressor.joblib')
+# Import python files
+import yieldPrediction.weatherAPI as wapi
+import yieldPrediction.prediction_model as pm
 
 # Function to make predictions on new data
 def predictor(request):
@@ -11,27 +12,35 @@ def predictor(request):
         crop_type = request.POST['crop_type']
         date = request.POST['date']
         recorded_extent = request.POST['crop_extent']
-        temperature = request.POST['temperature']
-        daylight = request.POST['daylight']
-        sunshine = request.POST['sunshine']
-        rain_sum = request.POST['rain_sum']
-        precipitation_hours = request.POST['precipitation_hours']
-        shortwave_radiation_sum = request.POST['shortwave_radiation_sum']
-        evapotranspiration = request.POST['evapotranspiration']
 
-        # Convert to Numeric Data Type
-        recorded_extent_numeric = float(recorded_extent)
-        temperature_numeric = float(temperature)
-        daylight_numeric = float(daylight)
-        sunshine_numeric = float(sunshine)
-        rain_sum_numeric = float(rain_sum)
-        precipitation_hours_numeric = float(precipitation_hours)
-        shortwave_radiation_sum_numeric = float(shortwave_radiation_sum)
-        evapotranspiration_numeric = float(evapotranspiration)
-    
-        y_pred = model.predict([[recorded_extent_numeric, temperature_numeric, daylight_numeric, sunshine_numeric, rain_sum_numeric, precipitation_hours_numeric, shortwave_radiation_sum_numeric, evapotranspiration_numeric]])
+        weather_data = wapi.processWeatherData()
+
+        average_temperature = weather_data[0]
+        daylight_sum = weather_data[1]
+        sunshine_sum = weather_data[2]
+        rain_sum = weather_data[3]
+        average_precipitation = weather_data[4]
+        average_radiation = weather_data[5]
+        average_evapotranspiration = weather_data[6]
+
+        #pd dataframe that consists of ml features
+        average_data = pd.DataFrame ({
+            'recorded extent': [recorded_extent],
+            'Temperature': [average_temperature],
+            'daylight(s)': [daylight_sum],
+            'sunshine(s)': [sunshine_sum],
+            'rain_sum (mm)': [rain_sum],
+            'precipitation_hours (h)': [average_precipitation],
+            'shortwave_radiation_sum (MJ/mÂ²)': [average_radiation],
+            'et0_fao_evapotranspiration (mm)': [average_evapotranspiration]
+        })
+
+        scaled_new_data = pm.scale_data(average_data, pm.scaler)
+
+        # Make predictions on the scaled new data
+        new_predictions = pm.make_predictions(scaled_new_data, pm.model)
         
-        return render(request, 'main.html', {'result' : y_pred})
+        return render(request, 'main.html', {'result' : new_predictions})
 
     return render(request, 'main.html')
     
