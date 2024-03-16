e5db6faa80d8bc975fc3f231bd99c151045ac3e7
Dasun Tharuka <dasuntharuk2002@gmail.com>
2024-03-16 10:00:49 +0530

    Clean

--- Backend/yieldPrediction/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from db_connection import db
-
-# Create your models here.
-user_collection = db['User']
-yield_collection = db['Crop_Yield']
-excess_collection = db['Crop_Excess']
