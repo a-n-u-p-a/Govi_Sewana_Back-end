108327c072dec086d9b6b1220e0da1ef8580269e
unknown <awanigasinghe2002@gmail.com>
2024-03-16 14:15:48 +0530

    changes to settings

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'yieldPrediction'
-    'userLogin',
+    'yieldPrediction',
+    'govisewana_login',
 ]
 
 MIDDLEWARE = [
--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('yieldPrediction/', include('yieldPrediction.urls')),
+    path('', include('govisewana_login.urls')),
 ]
