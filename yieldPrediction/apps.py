c1d1c397419f12a1d53181fd4fb9c7338c1b3b43
unknown <awanigasinghe2002@gmail.com>
2024-03-19 22:50:18 +0530

    resolved conflicts

--- yieldPrediction/apps.py
+++ yieldPrediction/apps.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 from django.apps import AppConfig
 
 
 class YieldpredictionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'yieldPrediction'
-=======
-from django.apps import AppConfig
-
-
-class YieldpredictionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'yieldPrediction'
->>>>>>> 7ef29c090b6300e9d7ec70a747fd81796baea790
--- yieldPrediction/models.py
+++ yieldPrediction/models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.db import models
 from db_connection import db
 
@@ -6,8 +5,3 @@ from db_connection import db
 user_collection = db['User']
 yield_collection = db['Crop_Yield']
 excess_collection = db['Crop_Excess']
-=======
-from django.db import models
-
-# Create your models here.
->>>>>>> 7ef29c090b6300e9d7ec70a747fd81796baea790
