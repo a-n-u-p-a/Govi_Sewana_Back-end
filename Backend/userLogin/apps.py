63dcbef080bd4f3bdb00560d488972f25fa3d204
unknown <awanigasinghe2002@gmail.com>
2024-03-23 17:53:52 +0530

    added twilio authentication and made changes

--- Backend/userLogin/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GovisewanaLoginConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'govisewana_login'
---
+++ signup/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('register_details/', views.register_details, name='register_details'),
+
+]
